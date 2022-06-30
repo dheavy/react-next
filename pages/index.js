fdde2a49f9c11f046bbb32f5e8a9cc6661155924
Davy Peter BRAUN <hello@davybraun.com>
2022-06-30 14:33:31 +0200

    feat: add pre-rendering and data-fetching examples

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,38 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  console.log(allPostsData)
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>{ siteTitle }</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section className={utilStyles.headingMd }>
         <p>It's just me refreshing a bit on Next.js!</p>
+      </section>
+      <section className={`${ utilStyles.headingMd } ${ utilStyles.padding1px }`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem } key={id}>
+              { title }<br />
+              { id }<br />
+              { date }
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
